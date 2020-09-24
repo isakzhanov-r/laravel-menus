221931a3ae35b491e0e8fdb2a129b8d4e537ae71
isakzhanovr <isakzhanov.r@gmail.com>
2020-09-24 11:24:23 +0300

    added a service for adding menu wrapping and menu items to html tags

---
+++ src/Services/WrapperAttributesService.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace IsakzhanovR\Menus\Services;
+
+
+use IsakzhanovR\Menus\Contracts\WrapperAttributesContract;
+
+final class WrapperAttributesService implements WrapperAttributesContract
+{
+}
