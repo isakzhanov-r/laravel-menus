264b9dc8cd2d285c10ab1c85f4cc9fa44401ed75
isakzhanov.r <rustam.isakzhanov@avangard-mb.ru>
2021-11-30 15:28:20 +0300

    Update config

--- config/settings.php
+++ config/settings.php
@@ -2,4 +2,15 @@
 
 return [
 
+    'menu' => [
+        'wrapper_tag'  => 'ul',
+        'class'        => 'menu',
+        'active_class' => 'active',
+    ],
+
+    'item' => [
+        'wrapper_tag'  => 'li',
+        'class'        => 'item',
+        'active_class' => 'active-item',
+    ],
 ];
