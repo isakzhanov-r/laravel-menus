434f688757508cf0a7c85f123d946814fbebff62
isakzhanov.r <rustam.isakzhanov@avangard-mb.ru>
2021-12-08 13:09:45 +0300

    update Interface for html wrapper service

--- src/Contracts/HtmlWrappersContract.php
+++ src/Contracts/HtmlWrappersContract.php
@@ -4,4 +4,9 @@ namespace IsakzhanovR\Menus\Contracts;
 
 interface HtmlWrappersContract
 {
+    public function wrap(string $tag, array $attributes = []);
+
+    public function beforeHtml($item);
+
+    public function afterHtml($item);
 }
